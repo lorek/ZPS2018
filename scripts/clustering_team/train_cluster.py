0167d07f067aff7108987a86c0063926bc87f6f2
kartofl <s246062@math.uni.wroc.pl>
2019-01-09 07:59:34 +0100

    PCA possible in clustering + minor fixes.

--- scripts/clustering_team/train_cluster.py
+++ scripts/clustering_team/train_cluster.py
@@ -12,7 +12,7 @@ import argparse
 import glob, os
 import pickle
 
-ALLOWED_ALGOS = ["Kmeans","GaussianMixture"]
+ALLOWED_ALGOS = ["kmeans","gmm"]
 SUBSETS = ["train", "validate"]
 INFI = 2000000000
 
@@ -22,10 +22,14 @@ def ParseArguments():
                             default = "features/cats_dogs/sifts/",
                             required = False,
                             help = 'data set directory name (features/cats_dogs/sifts/)')
+    parser.add_argument('--pca-d',
+                            default = 0,
+                            required = False,
+                            help = 'dimensions of PCA reduction if present (def. 0)')
     parser.add_argument('--algorithm',
                             default = "kmeans",
                             required = False,
-                            help='clustering alg. One of [kmeans] (def. kmeans)')
+                            help='clustering alg. One of [kmeans, gmm] (def. kmeans)')
     parser.add_argument('--k',
                             default = 100,
                             required = False,
@@ -38,10 +42,13 @@ def ParseArguments():
 
     args = parser.parse_args()
 
-    return args.features_dir, args.algorithm, int(args.k), int(args.knn)
+    return args.features_dir, int(args.pca_d), args.algorithm, int(args.k), int(args.knn)
 
 def load_class_features(class_path):
-    pickle_path = class_path + "/feats.pickle"
+    pca_d_str = ""
+    if pca_d > 0:
+        pca_d_str = str(pca_d)
+    pickle_path = class_path + "/" + pca_d_str + "feats.pickle"
 
     try:
         feats_file = open(pickle_path, 'rb')
@@ -60,7 +67,11 @@ def load_class_features(class_path):
 
 # Process args
 
-feats_dir, algorithm, k_par, knn  =  ParseArguments()
+feats_dir, pca_d, algorithm, k_par, knn  =  ParseArguments()
+
+if pca_d < 0:
+    print("Number of PCA's dimensions can't be negative. {} given as argument.").format(pca_d)
+    exit(4)
 
 if algorithm not in ALLOWED_ALGOS:
     print("Algorithm {} not allowed. Try one of {}".format(algorithm, ALLOWED_ALGOS))
@@ -69,7 +80,10 @@ if algorithm not in ALLOWED_ALGOS:
 # Load all features
 
 try:
-    feats_path = feats_dir + "sifts_all.pickle"
+    pca_d_str = ""
+    if pca_d > 0:
+        pca_d_str = str(pca_d)
+    feats_path = feats_dir + "sifts_all_" + pca_d_str + ".pickle"
     feats_file = open(feats_path, "rb")
 except:
     print("Couldn't open features file: {}.".format(feats_path))
@@ -83,24 +97,20 @@ feats_file.close()
 cluster = None
 params_str = ""
 
-if algorithm == "Kmeans":
-    object = KMeans(k_par) # numpy's k-means requires it
-    object.fit(all_feats)
+if algorithm == "kmeans":
+    cluster = KMeans(k_par) # numpy's k-means requires it
+    cluster.fit(all_feats)
     params_str += str(k_par)
 	
-if algorithm == "GaussianMixture":
-    object = GaussianMixture(k_par) # numpy's gmm's requires it
-    object.fit(all_feats)
+if algorithm == "gmm":
+    cluster = GaussianMixture(k_par) # numpy's gmm's requires it
+    cluster.fit(all_feats)
     params_str += str(k_par)
-
 
 cluster_path = "dict/" + "/".join(feats_dir.split("/")[1:3]) + "/" + algorithm + params_str + "/"
-	
-	
 
 if not os.path.exists(cluster_path):
        os.makedirs(cluster_path)
-
 
 # Load features per utterance (image)
 
@@ -111,7 +121,7 @@ for subs in SUBSETS:
         feats_per_utt = load_class_features(class_path)
 
         for utt in feats_per_utt.keys():
-            utt_splt = list(filter(None, utt.split("/"))) # remove empty strings (coz of ///// in paths)
+            utt_splt = list(filter(None, utt.replace("\\", "/").split("/"))) # remove empty strings (coz of ///// in paths)
             no_ext = ".".join(utt_splt[-1].split(".")[:-1])
             utt_path = cluster_path + "/knn" + str(knn) + "/" + "/".join(utt_splt[2:-1]) + "/"
 
@@ -123,7 +133,7 @@ for subs in SUBSETS:
             hist = [0] * k_par
 
             for feat in feats_per_utt[utt]:
-                hist[object.predict([feat])] += 1
+                hist[cluster.predict([feat])[0]] += 1
 
             for i in range(k_par):
                 out_clustered.write(str(hist[i]) + '\n')
